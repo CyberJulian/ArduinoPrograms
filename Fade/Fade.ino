165abbac7a1bd1e54e4f6841c23a598d46db5bcf
Will <cyberwjulian@gmail.com>
2022-02-24 12:59:44 -0500

    Fade LED with pin 9 and 6

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -20,16 +20,22 @@ int led = 9;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
+int aled = 6;           // the PWM pin the LED is attached to
+
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   pinMode(led, OUTPUT);
+  // declare pin 6 to be an output:
+  pinMode(aled, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
   analogWrite(led, brightness);
+// set the brightness of pin 9:
+  analogWrite(aled, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
